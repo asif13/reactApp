bf56b5e18541d86c783cda2fb42a83f7b29558c2
Asif Junaid <asif.junaid@nsiglobal.com>
2016-12-27 16:23:04 +0400

    styling detail view of album

--- reactApp/server/index.js
+++ reactApp/server/index.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 1337;
 const httpServer = require('http').createServer(app);
 
 const albumList = [{ artist: 'Muse', image: './images/helisnki1.jpg', title: 'here we go again', url: 'https://google.com' },
-{ artist: 'Muse', image: './images/helisnki1.jpg', title: 'here we go again', url: 'https://google.com' }];
+{ artist: 'Muse', image: './images/helisnki1.jpg', title: 'Its gonna be ', url: 'https://google.com' },
+{ artist: 'Muse', image: './images/helisnki1.jpg', title: 'Not gonna stop ', url: 'https://google.com' }];
 
 app.get('/albumList', (req, res) => {
   res.status(200).send(albumList);
